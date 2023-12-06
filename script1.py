c642dc1df9f89fdab4b4ab4d1b7841049eef6e10
Neylan Atac <natac@umich.edu>
2023-12-06 16:26:20 -0500

    Initialization

---
+++ script1.py
@@ -0,0 +1,27 @@
+import seaborn as sns
+import seaborn.objects as so
+
+anscombe = sns.load_dataset('anscombe')
+
+print(anscombe.head())
+
+(
+    anscombe
+    .groupby('dataset')
+    .agg(['mean', 'std'])
+)
+
+(
+    so.Plot(anscombe,
+            x='x',
+            y='y',
+           )
+    .add(so.Dot())
+    .facet('dataset', wrap=2)
+    .save("./figures/plot.png", dpi=200)
+)
+
+
+
+
+# print(sns.__version__)
